3ba83d7d6c8c2d06f63661e7e6d35da6aa920270
Cecile <cecile.lehoux@laposte.net>
2022-02-02 12:02:26 +0100

    ajout script

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const setupRoutes = require("./routes");
 require("dotenv").config();
 
 const app = express();
 
 // pre-route middlewares
-app.use(cors({ credentials: true}));
+// app.use(cors({ credentials: true}));
 app.use(express.json());
 
 // routes
 setupRoutes(app);
 
+// Please keep this module.exports app, we need it for the tests !
+module.exports = app;
 
---
+++ index.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, (err) => {
+  // eslint-disable-next-line no-console
+  if (err) console.error(err);
+  // eslint-disable-next-line no-console
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
